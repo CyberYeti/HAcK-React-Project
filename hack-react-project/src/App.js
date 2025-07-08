b3564258efd372e53654e26b798896e3a16182d7
CyberYeti <yitmengchin@gmail.com>
2025-07-08 15:42:44 -0700

    Added a joke mode that makes the text spin and move

--- hack-react-project/src/App.js
+++ hack-react-project/src/App.js
@@ -1,35 +1,49 @@
 import YMPic from './Yit-Meng_Pic.jpg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isFunnyActive, setFunny] = useState(false);
+
+  function toggleFunny(){
+    setFunny(!isFunnyActive)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={YMPic} className="App-logo" alt="logo" />
-        <p className="Name-Text">
-          Yit-Meng Chin
-        </p>
-        <p>
-          Hello, my name is Yit-Meng, I am a CS major. <br/>
-          I enjoy reading books, solving rubiks cubes, <br/>
-          and building things with my CAD and 3D printing skills.<br/>
-          The creation I am most proud of is my self sorting connect 4 
-          I call <a className="App-link"
-          href="https://makerworld.com/en/models/737254-cascade-connect-a-self-sorting-connect-4#profileId-669364"
-          target="_blank"
-          rel="noopener noreferrer">
-            Cascade Connect
-          </a>
-          .
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <div className= {isFunnyActive ? 'spin': 'normal'}>
+          <img src={YMPic} className="App-logo" alt="logo" />
+        </div>
+        <div className= {isFunnyActive ? 'squareMove': 'normal'}>
+          <div className= {isFunnyActive ? 'rspin': 'normal'}>
+            <div className= {isFunnyActive ? 'rainbowFast' : 'normal'}>
+              <p className="Name-Text">
+                Yit-Meng Chin
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className= {isFunnyActive ? 'squareMove2': 'normal'}>
+          <div className= {isFunnyActive ? 'spin' : 'normal'}>
+            <p className= {isFunnyActive ? 'rainbow' : 'normal'}>
+              Hello, my name is Yit-Meng, I am a CS major. <br/>
+              I enjoy reading books, solving rubiks cubes, <br/>
+              and building things with my CAD and 3D printing skills.<br/>
+              The creation I am most proud of is my self sorting connect 4 
+              I call <a className="App-link"
+              href="https://makerworld.com/en/models/737254-cascade-connect-a-self-sorting-connect-4#profileId-669364"
+              target="_blank"
+              rel="noopener noreferrer">
+                Cascade Connect
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+        <button className = 'button' onClick={toggleFunny}>
+          {isFunnyActive ? 'Disable Funny Mode' : 'Enable Funny Mode'}
+        </button>
       </header>
     </div>
   );
