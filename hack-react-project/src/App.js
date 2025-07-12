1eb9cfdaed17c17a914e3db29607b50004179797
CyberYeti <yitmengchin@gmail.com>
2025-07-11 17:51:19 -0700

    Added back body text

--- hack-react-project/src/App.js
+++ hack-react-project/src/App.js
@@ -13,6 +13,40 @@ function App() {
   return (
     <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
       <header className="App-header">
+        <div className= {isFunnyActive ? 'spin': 'normal'}>
+          <img src={YMPic} className="App-logo" alt="logo" />
+        </div>
+        <div className= {isFunnyActive ? 'squareMove': 'normal'}>
+          <div className= {isFunnyActive ? 'rspin': 'normal'}>
+            <div className= {isFunnyActive ? 'rainbowFast' : 'normal'}>
+              <p className="Name-Text">
+                Yit-Meng Chin
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className= {isFunnyActive ? 'squareMove2': 'normal'}>
+          <div className= {isFunnyActive ? 'spin' : 'normal'}>
+            <p className= {isFunnyActive ? 'rainbow' : 'normal'}>
+              Hello, my name is Yit-Meng, I am a CS major. <br/>
+              I enjoy reading books, solving rubiks cubes, <br/>
+              and building things with my CAD and 3D printing skills.<br/>
+              The creation I am most proud of is my self sorting connect 4 
+              I call <a className="App-link"
+              href="https://makerworld.com/en/models/737254-cascade-connect-a-self-sorting-connect-4#profileId-669364"
+              target="_blank"
+              rel="noopener noreferrer">
+                Cascade Connect
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+        <button className = 'button' onClick={toggleFunny}>
+          {isFunnyActive ? 'Disable Funny Mode' : 'Enable Funny Mode'}
+        </button>
+      </header>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello! My name is Cristian Aguilar and I am a Mechanical Engineering major. My favorite hobbies include going to the gym, swimming, and learning new instruments.
